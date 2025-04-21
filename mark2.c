327494ac5ca321862f3e721ae4233da71d31677c
Sandi <sandiforwork@gmail.com>
2025-04-21 16:58:40 +0800

    第六周作業更動

--- mark2.c
+++ mark2.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
-long long x ,y ,ans;
-int m;
+#define MAX_NUM 1000000 // 假設 a[i] 不超過 10^6
 
-int main(){
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
 
-    scanf("%lld %lld %d", &x ,&y ,&m);
-
-    if(y == 0){
-        printf("%lld",1 % m);
-        return 0;
-    }
-
-    ans = 1;
-    x = x % m;
-    
-    while (y > 0){
-        if(y % 2 == 1){
-            ans = (ans * x) % m;//ans = 二進制對應1的模數
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[100];
+    int count[MAX_NUM + 1] = {0}; // 記錄每個因數的出現次數
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        // 計算 a[i] 的因數
+        for (int j = 1; j * j <= a[i]; j++) {
+            if (a[i] % j == 0) {
+                count[j]++;
+                if (j * j != a[i]) {
+                    count[a[i] / j]++;
+                }
+            }
         }
-        x = (x * x) % m;//x 會一直平方取模數(但二進制是0的位置不會累乘進ans)
-        y /= 2;
     }
-    printf("%lld",ans);
+    
+    int max_gcd = 1;
+    for (int i = 1; i <= MAX_NUM; i++) {
+        if (count[i] >= 2 && i > max_gcd) {
+            max_gcd = i;
+        }
+    }
+    
+    printf("%d\n", max_gcd);
     return 0;
 }
 
